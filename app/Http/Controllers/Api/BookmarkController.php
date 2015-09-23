b43a7ef8354a848b0ce733b5c3f329686027de25
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2015-09-23 13:20:49 -0400

    Adds API comments

--- app/Http/Controllers/Api/BookmarkController.php
+++ app/Http/Controllers/Api/BookmarkController.php
@@ -15,29 +15,66 @@ use App\Utilities\ApiResponse;
 class BookmarkController extends Controller
 {
 
+    /**
+     * @api{post} /v1/bookmarks
+     * @apiDescription Creates a new bookmark.
+     * @apiGroup Bookmark
+     * @apiName CreateBookmark
+     */
     public function create(Request $req) {
         $bookmarkStatus = BookmarkService::create($req);
         return ApiResponse::fromStatus($bookmarkStatus);
     }
 
+    /**
+     * @api{get} /v1/bookmarks
+     * @apiDescription Gets a list of bookmarks the user has some access to.
+     * @apiGroup Bookmark
+     * @apiName GetBookmarks
+     */
     public function index(Request $req) {   
         return ApiResponse::fromStatus(BookmarkService::getMultiple($req));
     }
 
+    /**
+     * @api{get} /v1/bookmarks/:id
+     * @apiDescription Gets a single bookmark.
+     * @apiGroup Bookmark
+     * @apiName GetBookmark
+     */
     public function get($id) {
         return ApiResponse::fromStatus(BookmarkService::get($id));
     }
 
+    /**
+     * @api{delete} /v1/bookmarks/:id
+     * @apiDescription Deletes a single bookmark if the user has write permission.
+     * @apiGroup Bookmark
+     * @apiName DeleteBookmark
+     */
     public function delete($id) {
         $status = BookmarkService::delete($id);
         return ApiResponse::fromStatus($status);
     }
 
+    /**
+     * @api{put} /v1/bookmarks/:id
+     * @apiDescription Updates a bookmark if the user has write permission.
+     * @apiGroup Bookmark
+     * @apiName UpdateBookmark
+     */
     public function update(Request $req, $id) {
         $bookmarkStatus = BookmarkService::update($req, $id);
         return ApiResponse::fromStatus($bookmarkStatus);
     }
 
+    /**
+     * @api{put} /v1/bookmarks/:id/move
+     * @apiDescription Moves the bookmark to another project if the user 
+     * has write permission on both projects.
+     * @apiGroup Bookmark
+     * @apiName MoveBookmark
+     */
     public function move(Request $req, $id) {
         $bookmarkStatus = BookmarkService::move($req, $id);
         return ApiResponse::fromStatus($bookmarkStatus);
--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -12,21 +12,45 @@ use App\Utilities\ApiResponse;
 
 class ProjectController extends Controller
 {
+	/**
+	 * @api{get} /v1/projects
+	 * @apiDescription Returns a list of projects which the user has membership.
+	 * @apiGroup Project
+	 * @apiName GetProjects
+	 */
     function index() {
         $projects = ProjectService::getMultiple();
         return ApiResponse::fromResult($projects);
     }
 
+    /**
+	 * @api{get} /v1/projects/:id
+	 * @apiDescription Returns a single project and the user's membership.
+	 * @apiGroup Project
+	 * @apiName GetProject
+	 */
     function get($id) {
         $projectStatus = ProjectService::get($id);
         return ApiResponse::fromStatus($projectStatus);
     }
 
+    /**
+	 * @api{post} /v1/projects/
+	 * @apiDescription Creates a single project and sets the user as owner.
+	 * @apiGroup Project
+	 * @apiName CreateProject
+	 */
     function create(Request $req) {
         $status = ProjectService::create($req);
         return ApiResponse::fromStatus($status);
     }
 
+    /**
+	 * @api{delete} /v1/projects/:id
+	 * @apiDescription Deletes a project if the user is the owner.
+	 * @apiGroup Project
+	 * @apiName DeleteProject
+	 */
     function delete(Request $req, $id) {
     	$status = ProjectService::delete($req, $id);
     	return ApiResponse::fromStatus($status);
