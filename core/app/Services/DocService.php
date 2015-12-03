895388a1c33926bcda0a95d882e33d0a12213477
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2015-12-03 01:17:27 -0500

    Handle when document server is down

--- core/app/Services/DocService.php
+++ core/app/Services/DocService.php
@@ -24,84 +24,89 @@ class DocService {
 
 	public function create($args) {
 		if (!$this->active) return Status::fromError('Documents are disabled');
+		try {
+			$validator = Validator::make($args, [
+				'title' => 'required|string',
+				'project_id' => 'required|exists:projects,id'
+				]);
+			if ($validator->fails()) return Status::fromValidator($validator);
 
-		$validator = Validator::make($args, [
-			'title' => 'required|string',
-			'project_id' => 'required|exists:projects,id'
-			]);
-		if ($validator->fails()) return Status::fromValidator($validator);
+			$projectId = $args['project_id'];
+			$memberStatus = $this->memberService->checkPermission($projectId, 'w', $this->user);
+			if (!$memberStatus->isOK()) return $memberStatus;
 
-		$projectId = $args['project_id'];
-		$memberStatus = $this->memberService->checkPermission($projectId, 'w', $this->user);
-		if (!$memberStatus->isOK()) return $memberStatus;
+			// Begin a transaction in case there are errors with Etherpad.
+			DB::beginTransaction();
+			$doc = new Doc($args);
+			$doc->save();
 
-		// Begin a transaction in case there are errors with Etherpad.
-		DB::beginTransaction();
-		$doc = new Doc($args);
-		$doc->save();
+			// Create a group from the project id.
+			$response = $this->client->createGroupIfNotExistsFor($projectId);
+			if ($response->getCode() != 0) {
+				DB::rollback();
+				return Status::fromError('Could not create document');
+			}
+	        $groupId = $response->getData()['groupID'];
 
-		// Create a group from the project id.
-		$response = $this->client->createGroupIfNotExistsFor($projectId);
-		if ($response->getCode() != 0) {
-			DB::rollback();
-			return Status::fromError('Could not create document');
+			// Create a pad from the document id.
+			$padId = '' . $doc->id;
+			$response = $this->client->createGroupPad($groupId, $padId);
+			$response = $this->client->createPad($padId, '');
+			if ($response->getCode() != 0) {
+				DB::rollback();
+				return Status::fromError('Could not create document');
+			}
+
+			$doc->etherpad_group_id = $groupId;
+			$doc->project_id = $args['project_id'];
+			$doc->creator_id = $this->user->id;
+			$doc->save();
+			DB::commit();
+			return Status::fromResult($doc);
+		} catch (\Exception $e) {
+			return Status::fromError('Cannot connect to document service');
 		}
-        $groupId = $response->getData()['groupID'];
-
-		// Create a pad from the document id.
-		$padId = '' . $doc->id;
-		$response = $this->client->createGroupPad($groupId, $padId);
-		$response = $this->client->createPad($padId, '');
-		if ($response->getCode() != 0) {
-			DB::rollback();
-			return Status::fromError('Could not create document');
-		}
-
-		$doc->etherpad_group_id = $groupId;
-		$doc->project_id = $args['project_id'];
-		$doc->creator_id = $this->user->id;
-		$doc->save();
-		DB::commit();
-		return Status::fromResult($doc);
 	}
 
 	public function getWithSession($args) {
 		if (!$this->active) return Status::fromError('Documents are disabled');
+		try {
+			$validator = Validator::make($args, [
+				'doc_id' => 'required|integer'
+				]);
+			if ($validator->fails()) return Status::fromValidator($validator);
 
-		$validator = Validator::make($args, [
-			'doc_id' => 'required|integer'
-			]);
-		if ($validator->fails()) return Status::fromValidator($validator);
+			$doc = Doc::find($args['doc_id']);
+			if (is_null($doc)) return Status::fromError('Document not found', StatusCodes::NOT_FOUND);
 
-		$doc = Doc::find($args['doc_id']);
-		if (is_null($doc)) return Status::fromError('Document not found', StatusCodes::NOT_FOUND);
+			$memberStatus = $this->memberService->checkPermission($doc->project_id, 'w', $this->user);
+			if (!$memberStatus->isOK()) return $memberStatus;
 
-		$memberStatus = $this->memberService->checkPermission($doc->project_id, 'w', $this->user);
-		if (!$memberStatus->isOK()) return $memberStatus;
+			$response = $this->client->createAuthorIfNotExistsFor($this->user->id, $this->user->name);
+			if ($response->getCode() != 0) {
+				return Status::fromError('Could not create document user');
+			}
+			$authorId = $response->getData()['authorID'];
 
-		$response = $this->client->createAuthorIfNotExistsFor($this->user->id, $this->user->name);
-		if ($response->getCode() != 0) {
-			return Status::fromError('Could not create document user');
+	        $response = $this->client->createSession(
+	        	$doc->etherpad_group_id,
+	        	$authorId,
+	        	time() + $this->sessionTime);
+
+	        if ($response->getCode() !== 0) {
+	        	return Status::fromError('Could not create document session');
+	        }
+	        $sessionID = $response->getData()['sessionID'];
+
+			setcookie('sessionID', $sessionID);
+			return Status::fromResult($doc);
+		} catch (\Exception $e) {
+			return Status::fromError('Cannot connect to document service');
 		}
-		$authorId = $response->getData()['authorID'];
-
-        $response = $this->client->createSession(
-        	$doc->etherpad_group_id,
-        	$authorId,
-        	time() + $this->sessionTime);
-
-        if ($response->getCode() !== 0) {
-        	return Status::fromError('Could not create document session');
-        }
-        $sessionID = $response->getData()['sessionID'];
-
-		setcookie('sessionID', $sessionID);
-		return Status::fromResult($doc);
 	}
 
 	public function get($docId) {
 		if (!$this->active) return Status::fromError('Documents are disabled');
-
 		$validator = Validator::make($args, [
 			'doc_id' => 'required|integer'
 			]);
@@ -138,26 +143,29 @@ class DocService {
 
 	public function delete($docId) {
 		if (!$this->active) return Status::fromError('Documents are disabled');
+		try {
+			$validator = Validator::make(['doc_id' => $docId], [
+				'doc_id' => 'required|integer'
+				]);
+			if ($validator->fails()) return Status::fromValidator($validator);
 
-		$validator = Validator::make(['doc_id' => $docId], [
-			'doc_id' => 'required|integer'
-			]);
-		if ($validator->fails()) return Status::fromValidator($validator);
+			$doc = Doc::find($docId);
+			if (is_null($doc)) return Status::fromError('Document not found', StatusCodes::NOT_FOUND);
 
-		$doc = Doc::find($docId);
-		if (is_null($doc)) return Status::fromError('Document not found', StatusCodes::NOT_FOUND);
+			$memberStatus = $this->memberService->checkPermission($doc->project_id, 'w', $this->user);
+			if (!$memberStatus->isOK()) return $memberStatus;
 
-		$memberStatus = $this->memberService->checkPermission($doc->project_id, 'w', $this->user);
-		if (!$memberStatus->isOK()) return $memberStatus;
+			$padId = $this->getPadId($doc);
+	        $response = $this->client->deletePad($padId);
+	        if ($response->getCode() != 0) {
+	        	return Status::fromError('Could not delete document');
+	        }
 
-		$padId = $this->getPadId($doc);
-        $response = $this->client->deletePad($padId);
-        if ($response->getCode() != 0) {
-        	return Status::fromError('Could not delete document');
-        }
-
-        $doc->delete();
-		return Status::OK();
+	        $doc->delete();
+			return Status::OK();
+		} catch (\Exception $e) {
+			return Status::fromError('Cannot connect to document service');
+		}
 	}
 
 	public function getPadId($doc) {
